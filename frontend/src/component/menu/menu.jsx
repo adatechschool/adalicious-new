3c0911c7358f607d6c6640f6cda1279232ccf061
Ange-web <angenono.pro@gmail.com>
2025-04-10 00:07:31 +0200

    Update menu.jsx

--- frontend/src/component/menu/menu.jsx
+++ frontend/src/component/menu/menu.jsx
@@ -8,7 +8,7 @@ export default function ChoixMenu() {
   const navigate = useNavigate(); // <-- Hook ajouté
 
   useEffect(() => {
-    fetch('http://localhost:3000/plats')
+    fetch(`${import.meta.env.VITE_API_URL}/plats`)
       .then(res => res.json())
       .then(data => {
         console.log("Plats reçus :", data);
